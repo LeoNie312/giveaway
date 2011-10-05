abd811a64f9d99c1b09508ff33ef2396bf965f82
Adam Li <adam.lee0628@gmail.com>
2011-10-05 17:33:36 +0800

    Just set up the Category model

---
+++ app/models/category.rb
@@ -0,0 +1,13 @@
+class Category < ActiveRecord::Base
+end
+
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
