a3be3af39c046b0b412e196216a1445786300646
Adam Li <adam.lee0628@gmail.com>
2011-10-08 16:23:44 +0800

    Half done with the user homepage

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,4 +7,17 @@ Factory.define :item do |i|
   i.img_link     "http://www.example.com/photos/some_pic.jpg"
   i.association  :category
   i.onshelf       true
+  i.category_id   1
+end
+
+Factory.define :user do |u|
+  u.name     "Mark Zuckerberg"
+  u.email    "mark@e.ntu.edu.sg"
+  u.hp       "84257886"
+  u.password "foobar"
+  u.password_confirmation "foobar"
+end
+
+Factory.define :wish do |w|
+  
 end
