b1887ebb3e950c37c304edc32e5158c5a93e264d
Leo <leo.nie312@gmail.com>
2011-10-17 13:52:51 +0800

    seeded connections table

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,18 @@
    Category.find_or_create_by_name(category)
 end
 
+connections = {5=>1, 6=>1, # all base's children
+              4=>5, # all drink's children
+              2=>4, 3=>4, 9=>4, # all solf-drink's children
+              7=>6, # all stationary's children
+              8=>7 # all pen's children
+              }
+
+Connection.delete_all              
+connections.each do |child, parent|
+  Connection.create!(:parent_id=>parent, :child_id=>child)
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
