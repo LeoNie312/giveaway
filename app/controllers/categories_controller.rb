3e56da9ac4fabf8d5d53015526654f99153ce177
Leo <leo.nie312@gmail.com>
2011-10-18 14:45:58 +0800

    modified categories show page, now move on to create items controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,11 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by_id(params[:id])
     @title = @category.name.capitalize
+    
+    @items = []
+    @current_category = @category
+    @current_items = @current_category.items
+    @items.concat @current_items
   end
 
 end
--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -5,11 +5,14 @@ describe CategoriesController do
   describe "GET 'show'" do
     
     before :each do
-      @category = Factory(:category)
+      @base = Factory(:category)
+      @item_b1 = Factory(:item, :category_id=>@base.id, 
+                        :owner=>Factory(:user))
+                       
+      get :show, :id => @base
     end
     
     it "should be successful" do
-      get :show, :id => @category
       response.should be_success
     end
     
@@ -17,6 +20,14 @@ describe CategoriesController do
     #   get :show, :id => @category
     #   response.should have_selector("title", :content => @category.name.capitalize)
     # end
+    
+    it "should be the right category" do
+      assigns(:category).should == @base
+    end
+    
+    it "should have this category's items" 
+    
+    it "should have "
   end
 
 end
