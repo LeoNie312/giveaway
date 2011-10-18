63cc0b7232dfd06e4e479c9347c271e3c4f3f240
Leo <leo.nie312@gmail.com>
2011-10-18 22:05:06 +0800

    small changes, to validate connection model

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -2,4 +2,6 @@ class Connection < ActiveRecord::Base
   belongs_to :parent, :class_name => 'Category'
   belongs_to :child, :class_name => 'Category'
   
+  validates :parent_id, :presence => true
+  validates :child_id, :presence => true
 end
--- spec/models/connection_spec.rb
+++ spec/models/connection_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 describe Connection do
+  
+  before :each do
+    @parent_cate = Factory(:category)
+    @child_cate = Factory(:category, :name => "drink")
+  end
 
   describe "category association" do
     
     before :each do
-      @parent_cate = Factory(:category)
-      @child_cate = Factory(:category, :name => "drink")
       @connection = Connection.create(:parent_id=>@parent_cate.id, :child_id=>@parent_cate.id)
     end
     
@@ -18,6 +21,19 @@ describe Connection do
     it "should have a child method" do
       @connection.should respond_to(:child)
       @connection.child.should be_instance_of(Category)
+    end
+  end
+  
+  describe "validation" do
+    
+    it "should require parent id" do
+      Connection.new(:child_id => @child_cate.id)
+        .should_not be_valid
+    end
+    
+    it "should require child id" do
+      Connection.new(:parent_id => @child_cate.id)
+        .should_not be_valid
     end
   end
 end
