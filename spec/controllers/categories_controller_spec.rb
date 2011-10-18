7406b158351d527d3c4965fbea03a4e81f284f33
Leo <leo.nie312@gmail.com>
2011-10-18 20:07:55 +0800

    include children categories' items tomorrow

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe CategoriesController do
-
+  render_views
+  
   describe "GET 'show'" do
     
     before :each do
       @base = Factory(:category)
       @item_b1 = Factory(:item, :category_id=>@base.id, 
-                        :owner=>Factory(:user))
-                       
-      get :show, :id => @base
+                        :owner=>Factory(:user))                       
     end
     
     it "should be successful" do
+      get :show, :id => @base
       response.should be_success
     end
     
@@ -22,12 +22,25 @@ describe CategoriesController do
     # end
     
     it "should be the right category" do
+      get :show, :id => @base
       assigns(:category).should == @base
     end
     
-    it "should have this category's items" 
+    it "should have this category's items" do
+      get :show, :id => @base
+      response.should have_selector("span",
+                      :class => "category-tag",
+                      :content => @base.name.capitalize)
+    end
     
-    it "should have "
+    it "should have children category's items" do
+      @drink = Factory(:category, :name => "drink")
+      @connection1 = Factory(:connection, :parent_id => @base.id,
+                      :child_id => @drink.id)
+      response.should have_selector("span",
+                      :class=> "category-tag",
+                      :content => @drink.name.capitalize)
+    end
   end
 
 end
